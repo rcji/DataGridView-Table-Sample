814cd668a54517bac455b0ed57808363e5af2025
RCryeji <raphaelchase302@gmail.com>
2024-02-21 22:32:31 +0800

    Add project files.

---
+++ Osorio - Week 4 Assignment/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Osorio___Week_4_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.birthyearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxGender = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxBirthyear = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.ageDataGridViewTextBoxColumn,
+            this.genderDataGridViewTextBoxColumn,
+            this.birthyearDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.personBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(785, 371);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            this.ageDataGridViewTextBoxColumn.DataPropertyName = "Age";
+            this.ageDataGridViewTextBoxColumn.HeaderText = "Age";
+            this.ageDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            this.ageDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // genderDataGridViewTextBoxColumn
+            // 
+            this.genderDataGridViewTextBoxColumn.DataPropertyName = "Gender";
+            this.genderDataGridViewTextBoxColumn.HeaderText = "Gender";
+            this.genderDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
+            this.genderDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // birthyearDataGridViewTextBoxColumn
+            // 
+            this.birthyearDataGridViewTextBoxColumn.DataPropertyName = "Birthyear";
+            this.birthyearDataGridViewTextBoxColumn.HeaderText = "Birthyear";
+            this.birthyearDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.birthyearDataGridViewTextBoxColumn.Name = "birthyearDataGridViewTextBoxColumn";
+            this.birthyearDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // personBindingSource
+            // 
+            this.personBindingSource.DataSource = typeof(Osorio___Week_4_Assignment.Form1.Person);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 460);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(112, 40);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 414);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 40);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(505, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(498, 414);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(175, 22);
+            this.textBoxName.TabIndex = 4;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(498, 464);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(51, 22);
+            this.textBoxAge.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 445);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Age";
+            // 
+            // textBoxGender
+            // 
+            this.textBoxGender.Location = new System.Drawing.Point(555, 464);
+            this.textBoxGender.Name = "textBoxGender";
+            this.textBoxGender.Size = new System.Drawing.Size(118, 22);
+            this.textBoxGender.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(562, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Gender";
+            // 
+            // textBoxBirthyear
+            // 
+            this.textBoxBirthyear.Location = new System.Drawing.Point(679, 414);
+            this.textBoxBirthyear.Name = "textBoxBirthyear";
+            this.textBoxBirthyear.Size = new System.Drawing.Size(118, 22);
+            this.textBoxBirthyear.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(686, 395);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Birthyear";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(685, 446);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 40);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(130, 460);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(113, 40);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete Row";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 512);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBoxBirthyear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxGender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxGender;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxBirthyear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.BindingSource personBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn birthyearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Osorio - Week 4 Assignment/Form1.cs
@@ -0,0 +1,136 @@
+﻿using GemBox.Spreadsheet.WinFormsUtilities;
+using GemBox.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Osorio___Week_4_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+       
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }  
+            public int Birthyear { get; set; }
+
+        }
+
+
+        List<Person> people = new List<Person>();
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter =
+            "XLS files (*.xls, *.xlt)|*.xls;*.xlt|" +
+            "XLSX files (*.xlsx, *.xlsm, *.xltx, *.xltm)|*.xlsx;*.xlsm;*.xltx;*.xltm|" +
+            "ODS files (*.ods, *.ots)|*.ods;*.ots|" +
+            "CSV files (*.csv, *.tsv)|*.csv;*.tsv|" +
+            "HTML files (*.html, *.htm)|*.html;*.htm";
+            openFileDialog.FilterIndex = 2;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workbook = ExcelFile.Load(openFileDialog.FileName);
+                var worksheet = workbook.Worksheets.ActiveWorksheet;
+                // From ExcelFile to DataGridView.
+                DataGridViewConverter.ExportToDataGridView(
+                worksheet,
+                this.dataGridView1,
+                new ExportToDataGridViewOptions() { ColumnHeaders = true }); ;
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter =
+            "XLS (*.xls)|*.xls|" +
+            "XLT (*.xlt)|*.xlt|" +
+            "XLSX (*.xlsx)|*.xlsx|" +
+            "XLSM (*.xlsm)|*.xlsm|" +
+            "XLTX (*.xltx)|*.xltx|" +
+            "XLTM (*.xltm)|*.xltm|" +
+            "ODS (*.ods)|*.ods|" +
+            "OTS (*.ots)|*.ots|" +
+            "CSV (*.csv)|*.csv|" +
+            "TSV (*.tsv)|*.tsv|" +
+            "HTML (*.html)|*.html|" +
+            "MHTML (.mhtml)|*.mhtml|" +
+            "PDF (*.pdf)|*.pdf|" +
+            "XPS (*.xps)|*.xps|" +
+            "BMP (*.bmp)|*.bmp|" +
+            "GIF (*.gif)|*.gif|" +
+            "JPEG (*.jpg)|*.jpg|" +
+            "PNG (*.png)|*.png|" +
+            "TIFF (*.tif)|*.tif|" +
+            "WMP (*.wdp)|*.wdp";
+            saveFileDialog.FilterIndex = 3;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var workbook = new ExcelFile();
+                var worksheet = workbook.Worksheets.Add("Sheet1");
+                // From DataGridView to ExcelFile.
+                DataGridViewConverter.ImportFromDataGridView(
+                worksheet,
+                this.dataGridView1,
+                new ImportFromDataGridViewOptions() { ColumnHeaders = true });
+                workbook.Save(saveFileDialog.FileName);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            people.Add(new Person { Name = textBoxName.Text, 
+                                    Age = int.Parse(textBoxAge.Text),  
+                                    Gender = textBoxGender.Text, 
+                                    Birthyear = int.Parse(textBoxBirthyear.Text)});
+
+            //int rowCount = people.Count;
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = people;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a row to delete.");
+                return;
+            }
+
+            int selectedIndex = dataGridView1.SelectedRows[0].Index;
+
+            people.RemoveAt(selectedIndex);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = people;  
+        }
+    }
+}
